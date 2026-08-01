03953b1cba2e7b962243db6f033910eae3e492a9
ishane96 <achinthaishane96@gmail.com>
2026-08-01 13:40:55 +0100

    initial commit

---
+++ AbsenceReport/AbsenceReport/AbsenceReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceReportApp.swift
+//  AbsenceReport
+//
+//  Created by Achintha kahawalage on 2026-08-01.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
